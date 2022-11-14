b6a4abdc74c982383e2222284c1fbf914ffca034
verymadjack <verymadjacek@gmail.com>
2022-11-15 00:28:57 +0200

    Full Project1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+         $categories = Category::all();
+        $posts = Post::when(request('category_id'), function ($query) {
+                $query->where('category_id', request('category_id'));
+            })
+            ->latest()
+            ->get();
+
+        return view('index',
+            compact('categories', 'posts'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
+
+Route::get('posts/{post}',
+    [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
